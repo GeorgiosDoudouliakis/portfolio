ca8b8b5e846e1b4547c7e4c76a4de5356dfece82
Georgios Doudouliakis <dudugeorge89@gmail.com>
2021-12-03 19:28:31 +0200

    feat: Add layout and styling for intro component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ResumeComponent } from './components/resume/resume.component';
 import { AboutComponent } from './components/about/about.component';
 import { PdfViewComponent } from './components/pdf-view/pdf-view.component';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { IntroComponent } from './components/intro/intro.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
     FooterComponent,
     ResumeComponent,
     AboutComponent,
-    PdfViewComponent
+    PdfViewComponent,
+    IntroComponent
   ],
   imports: [
     BrowserModule,
