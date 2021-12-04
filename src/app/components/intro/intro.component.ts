fb99c6467b4dff8b0bfc13074bf9152ead9a6245
Georgios Doudouliakis <dudugeorge89@gmail.com>
2021-12-04 20:48:06 +0200

    feat: Add arrow in intro component when on click navigates to projects

--- src/app/components/intro/intro.component.ts
+++ src/app/components/intro/intro.component.ts
@@ -9,7 +9,9 @@ export class IntroComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  scrollToProjects() {
+    document.querySelector(`.projects`)?.scrollIntoView();
+  }
 }
