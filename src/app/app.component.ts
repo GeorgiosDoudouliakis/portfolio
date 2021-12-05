15b8c8dca40e6c8db6070d57bcd37204ad0b9c99
Georgios Doudouliakis <dudugeorge89@gmail.com>
2021-12-05 14:44:27 +0200

    feat: Add meta tags and set title of website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'portfolio';
+
+  constructor(private meta: Meta, private title: Title) {
+    this.meta.addTags([
+      { name: 'description', content: 'Portfolio website of George Doudouliakis, a Front End Web Developer' },
+      { name: 'author', content: 'George Doudouliakis' },
+      { name: 'keywords' , content: 'portfolio, George Doudouliakis, front end web development, web development' }
+    ])
+
+    this.title.setTitle('George Doudouliakis | Front End Web Developer');
+  }
 }
