0ba6d671aa953a1513991b30b802b892f4f1d845
Georgios Doudouliakis <dudugeorge89@gmail.com>
2021-12-05 18:36:02 +0200

    feat: Add preview link in covid-19 greece project

--- src/app/mock/projects.mock.ts
+++ src/app/mock/projects.mock.ts
@@ -5,7 +5,7 @@ export const PROJECTS: Project[] = [
         title: 'Covid-19 Greece',
         description: 'A webpage with stats and news about COVID-19 in Greece. The user has the ability to see a map with overall cases on each region in Greece and global news about COVID-19 for the country he will select.',
         tags: ['HTML', 'CSS', 'Bulma', 'JavaScript', 'TypeScript', 'Angular'],
-        previewLink: '',
+        previewLink: 'https://georgiosdoudouliakis.github.io/covid19-greece/',
         codeLink: 'https://github.com/GeorgiosDoudouliakis/covid19-greece'
     },
     {
