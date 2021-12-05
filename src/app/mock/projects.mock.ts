6e67f28d4e323663db8b10693e5e22153122e291
Georgios Doudouliakis <dudugeorge89@gmail.com>
2021-12-05 16:21:35 +0200

    feat: Add bulma as a tag in covid 19 greece project

--- src/app/mock/projects.mock.ts
+++ src/app/mock/projects.mock.ts
@@ -4,7 +4,7 @@ export const PROJECTS: Project[] = [
     {
         title: 'Covid-19 Greece',
         description: 'A webpage with stats and news about COVID-19 in Greece. The user has the ability to see a map with overall cases on each region in Greece and global news about COVID-19 for the country he will select.',
-        tags: ['HTML', 'CSS', 'JavaScript', 'TypeScript', 'Angular'],
+        tags: ['HTML', 'CSS', 'Bulma', 'JavaScript', 'TypeScript', 'Angular'],
         previewLink: '',
         codeLink: 'https://github.com/GeorgiosDoudouliakis/covid19-greece'
     }
