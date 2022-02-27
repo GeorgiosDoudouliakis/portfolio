9e3ea5d672dbc108fd2bc58971ac8916f632b07e
Georgios Doudouliakis <dudugeorge89@gmail.com>
2022-02-27 19:07:50 +0200

    refactor: Change the description of formula one project

--- src/app/mock/projects.mock.ts
+++ src/app/mock/projects.mock.ts
@@ -3,7 +3,7 @@ import { Project } from "../models/project.model";
 export const PROJECTS: Project[] = [
     {
         title: 'Formula One',
-        description: 'Website with stats about formula one from 1950 to 2021. Driver/Constructor details, standings, circuits, statistics showing points per circuit and map with the location of each circuit',
+        description: 'Website with stats about formula one from 1950 to 2021. Driver/Constructor details, standings, circuits, statistics showing points per circuit, map with the location of each circuit and schedule',
         tags: ['HTML', 'CSS', 'JavaScript', 'TypeScript', 'Angular', 'Angular Material', 'Open Layers', 'Apache Echarts'],
         previewLink: 'https://georgiosdoudouliakis.github.io/formula-one/',
         codeLink: 'https://github.com/GeorgiosDoudouliakis/formula-one'
