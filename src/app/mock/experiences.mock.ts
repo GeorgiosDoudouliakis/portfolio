d9d2e6be067cefb4f8358469efcbc4ecbb7a503a
Georgios Doudouliakis <dudugeorge89@gmail.com>
2022-02-27 19:24:51 +0200

    feat: Add latest experience

--- src/app/mock/experiences.mock.ts
+++ src/app/mock/experiences.mock.ts
@@ -2,6 +2,12 @@ import { Experience } from "../models/experience.model";
 
 export const EXPERIENCES: Experience[] = [
     {
+        company: 'Schoox Inc',
+        companyDescription: 'Corporate Learning Management Systems (LMS)',
+        location: 'Thessaloniki',
+        duration: 'March 2022 - present'
+    },
+    {
         company: 'Kiss My Button PC',
         companyDescription: 'Web Development Software House',
         location: 'Thessaloniki',
